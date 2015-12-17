d456730ad9fe294f323dd33681b6b8e27b244b2b
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-12-17 13:58:58 -0600

    text being broadcasted to all connections

--- client/scripts/editor.js
+++ client/scripts/editor.js
@@ -1,12 +1,36 @@
 // editor.js
 $(function() {
   $(document).ready(function() {
-    console.log('Ready!');
+    var conn;
+
+    if (window['WebSocket']) {
+      conn = new WebSocket('ws://localhost:8080/ws');
+      console.log('New websocket!');
+    } else {
+      alert('Your browser does not support WebSockets.');
+    }
+
     $('.editable').text('');
     var editor = new MediumEditor('.editable', {
       placeholder: {
         text: ''
       }
     });
+
+    var editorDiv = $('.editable');
+    $('.editable').on('input', function() {
+      var text = editorDiv.text();
+      if (typeof conn !== 'undefined' && text !== '') {
+        console.log(text);
+        conn.send(text);
+      }
+      return false;
+    });
+
+    if (typeof conn !== 'undefined') {
+      conn.onmessage = function(msg) {
+        $('.editable').text(msg.data);
+      };
+    }
   });
 })
